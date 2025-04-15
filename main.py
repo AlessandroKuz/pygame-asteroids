752fa5c526ed5c0642159377e84091ee9b93a84f
AlessandroKuz <akuzcontact@gmail.com>
2025-04-15 10:04:54 +0200

    feat: add player class and draw it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,11 +12,15 @@ def main():
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    my_player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        my_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
