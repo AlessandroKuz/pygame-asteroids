bc0e8cba3035d17e498bace4c9a121143470990d
AlessandroKuz <akuzcontact@gmail.com>
2025-04-15 10:22:02 +0200

    feat: add player movements and refactor into groups
    Add movements to the Player class, allowing it to rotate and go
    forward and backwards. Refacor the update and draw methods to use
    groups instead of directly the player.

--- main.py
+++ main.py
@@ -8,19 +8,25 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    print('Starting Asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     my_player = Player(x, y)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    return
+        updatable.update(dt)
         screen.fill('black')
-        my_player.draw(screen)
+        for element in drawable:
+            element.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
